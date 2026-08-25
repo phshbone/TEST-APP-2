19babec07dd89e9e21826a0d86152ea775e18a20
phshbone <phshbone@gmail.com>
2026-08-25 12:31:43 -0400

    Vendor Master Reference helper locally

---
+++ vendor/master-reference.js
@@ -0,0 +1,13 @@
+(function(){
+  const menu=document.getElementById('sideMenu');
+  if(!menu) return;
+  const button=document.createElement('button');
+  button.type='button';
+  button.textContent='Master Reference';
+  button.style.cssText='display:block;width:100%;text-align:left;min-height:50px;border:0;border-bottom:1px solid #d7e0eb;background:#fff;color:#123a6d;font-weight:800';
+  menu.appendChild(button);
+  button.onclick=()=>{
+    try{menu.close();}catch(e){}
+    alert('Master Reference shell is reserved for Morris technical material. Sensitive Morris manuals are not bundled in this public demo build.');
+  };
+})();
